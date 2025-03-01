a18e7a9672cf9fa4f54d0764f77d798890c0a62f
GaballaGit <mgaballa@csu.fullerton.edu>
2025-03-01 15:29:04 -0800

    Hashing

--- gabagit/cli.py
+++ gabagit/cli.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+
+from . import data
 
 
 def main():
@@ -15,9 +18,17 @@ def parse_args():
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
+
     return parser.parse_args()
 
 
 def init(args):
-    print('Hello world!')
+    data.init()
+    print(f'Initalized empty gabagit repository in {os.getcwd()}/{data.GIT_DIR}')
 
+def hash_objects(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
