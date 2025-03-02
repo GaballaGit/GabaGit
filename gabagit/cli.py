0397bd8e74a774cf563713f0b1934c110c8fbf53
GaballaGit <mgaballa@csu.fullerton.edu>
2025-03-01 22:35:54 -0800

    Add data tags, prepare base for higher logic structures

--- gabagit/cli.py
+++ gabagit/cli.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 
+from . import base
 from . import data
 
 
@@ -40,6 +41,6 @@ def hash_object(args):
 
 def cat_file(args):
     sys.stdout.flush()
-    sys.stdout.buffer.write(data.get_object(args.object))
+    sys.stdout.buffer.write(data.get_object(args.object, expected=None))
 
 
