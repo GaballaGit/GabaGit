c58f1404c91d884d4d1f5840658d6faa32bf0b46
GaballaGit <mgaballa@csu.fullerton.edu>
2025-03-02 17:05:34 -0800

    Comments

--- gabagit/base.py
+++ gabagit/base.py
@@ -2,20 +2,24 @@ import os
 
 from . import data
 
-
+# Write files to tree
 def write_tree(directory='.'):
     entries = []
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
+
+            # if .gabagit then ignore
             if is_ignored(full):
                 continue
 
+            # Write file
             if entry.is_file(follow_symlinks=False):
                 type_ = 'blob'
                 with open(full, 'rb') as f:
                     oid = data.hash_object(f.read())
 
+            # Write directory, then recurssion to write inside the dir
             elif entry.is_dir(follow_symlinks=False):
                 type_ = 'tree'
                 oid = write_tree(full)
@@ -27,22 +31,31 @@ def write_tree(directory='.'):
                     in sorted (entries))
     return data.hash_object(tree.encode(), 'tree')
 
+# Iterate through entries in tree
 def _iter_tree_entries(oid):
     if not oid:
         return
+
     tree = data.get_object(oid, 'tree')
     for entry in tree.decode().splitlines():
         type_, oid, name = entry.split(' ', 2)
+        # Yield returns an iteratable:
         yield type_, oid, name
+
 
 def get_tree(oid, base_path=''):
     result = {}
     for type_, oid, name in _iter_tree_entries(oid):
+        # Errors if file is incorrectly named
         assert '/' not in name
         assert name not in ('..', '.')
+
+        # /usr/path/name
         path = base_path + name
         if type_ == 'blob':
             result[path] = oid
+
+        # If dir, recurssion until we reach a blob type file
         elif type_ == 'tree':
             result.update(get_tree(oid, f'{path}/'))
         else:
@@ -51,6 +64,7 @@ def get_tree(oid, base_path=''):
 
 
 def read_tree(tree_oid):
+    # Make a dir with read tree
     for path, oid in get_tree(tree_oid, base_path='./').items():
         os.makedirs(os.path.dirname(path), exist_ok=True)
         with open(path, 'wb') as f:
